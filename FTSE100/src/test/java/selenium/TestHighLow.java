dcc77270c85b745528b65090e4bfc263cce7970c
Cristina <CLopezCanoyra@qa.com>
2020-10-12 10:15:18 +0100

    changes made last week to FTSE100 file

--- FTSE100/src/test/java/selenium/TestHighLow.java
+++ FTSE100/src/test/java/selenium/TestHighLow.java
@@ -4,9 +4,11 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
@@ -18,11 +20,14 @@ public class TestHighLow {
 	
 	private WebDriver driver;
 	
-	@Before
+	@BeforeClass
 	public void setup() {
 		 System.setProperty("webdriver.chrome.driver", "C:\\Users\\e13cr\\Documents\\workspace-spring-tool-suite-4-4.8.0.RELEASE\\selenium-day1\\src\\test\\resources\\driver\\chromedriver.exe");
 	        driver = new ChromeDriver();
 	        driver.manage().window().setSize(new Dimension(1366, 768));
+	        
+	        driver.manage().timeouts().pageLoadTimeout(2, TimeUnit.SECONDS);
+	        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 	}
 	
 	@Test
@@ -31,23 +36,21 @@ public class TestHighLow {
         List<WebElement> high = new ArrayList<WebElement>();
         List<String> store = new ArrayList<String>();
 		
-		driver.get("https://www.hl.co.uk/shares/stock-market-summary/ftse-100");
-        Thread.sleep(1500);
+		driver.get("https://www.hl.co.uk/shares/stock-market-summary/ftse-100/risers%22");
         
-        //Find the higher    
-        assistElement = driver.findElement(By.linkText("RISERS"));
-        assistElement.click();
-        Thread.sleep(2500);
+        high = driver.findElements(By.xpath("/html/body/main/div/div/div[3]/div/div[4]/div[1]/div/table/tbody/*"));
+        System.out.println("--" + high + "--");
         
-        high = driver.findElements(By.partialLinkText("%"));
+//        int count = 0;
+//        for (WebElement higher : high) {
+//        	store.add(higher.getText());
+//        	
+//        	count++;
+//        }
+//        
+//        System.out.println(store);
         
-        int count = 0;
-        for (WebElement higher : high) {
-        	store.add(higher.getText());
-        	count++;
-        }
         
-        Thread.sleep(20000);
         //assertEquals("Bing", driver.getTitle());
     }
 	
