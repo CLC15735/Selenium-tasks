03363720654640a4021dde3676579b69e7b88608
Cristina <CLopezCanoyra@qa.com>
2020-10-13 14:48:09 +0100

    Restored mercury tours

--- MercuryTours/src/test/java/selenium/TestLogIn.java
+++ MercuryTours/src/test/java/selenium/TestLogIn.java
@@ -10,138 +10,122 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.FindBy;
 
 public class TestLogIn {
 	
-	public static final String URL = "http://demo.guru99.com/test/newtours/";
+private WebDriver driver;
 	
+	@Before
+	public void setup() {
+		 System.setProperty("webdriver.chrome.driver", "C:\\Users\\e13cr\\Documents\\workspace-spring-tool-suite-4-4.8.0.RELEASE\\selenium-day1\\src\\test\\resources\\driver\\chromedriver.exe");
+	        driver = new ChromeDriver();
+	        driver.manage().window().setSize(new Dimension(1366, 768));
+	}
 	
-	
-	//Find the Create user section
-	@FindBy (xpath = "/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td[2]/a")
-	private WebElement createUserLink;
-	
-	// pages
-	public Register register;
+	@Test
+	public void test() throws InterruptedException {
+        WebElement assist;
+		
+        String name = "Elena";
+        String password = "12345";
+		driver.get("http://demo.guru99.com/test/newtours/");
 
+		//Find create user
+		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td[2]/a"));
+		assist.click();
+		Thread.sleep(1500);
+		
+			
+        //Username
+		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[13]/td[2]/input"));
+		assist.sendKeys(name);
+		Thread.sleep(1000);
+		
+		//Password
+		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[14]/td[2]/input"));
+		assist.sendKeys(password);
+		Thread.sleep(1000);
+		
+		//Confirm Password
+		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[15]/td[2]/input"));
+		assist.sendKeys(password);
+		Thread.sleep(1000);
+		
+		//Submit
+		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[17]/td/input"));
+		assist.click();
+		Thread.sleep(1000);
+		
+		//Sign in
+		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[3]/td/p[2]/font/a"));
+		assist.click();
+		Thread.sleep(1000);
+		
+		//Username
+		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[1]/td[2]/input"));
+		assist.sendKeys(name);
+		Thread.sleep(1000);
+		
+		//Password
+		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[2]/td[2]/input"));
+		assist.sendKeys(password);
+		Thread.sleep(1000);
+		
+		//Submit
+		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[4]/td/input"));
+		assist.click();
+		Thread.sleep(1000);
+
+		//Flight finder
+		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[1]/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[2]/td[2]/a"));
+		assist.click();
+		Thread.sleep(1000);
+		
+		//Number of passengers
+		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[3]/td[2]/b/select"));
+		assist.click();
+		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[3]/td[2]/b/select/option[2]"));
+		assist.click();
+		Thread.sleep(1000);
+		
+		//Departure from
+		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[5]/td[2]/select[1]"));
+		assist.click();
+		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[4]/td[2]/select/option[3]"));
+		assist.click();
+		Thread.sleep(1000);
+		
+		//Arriving to
+		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[6]/td[2]/select"));
+		assist.click();
+		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[6]/td[2]/select/option[5]"));
+		assist.click();
+		Thread.sleep(1000);
+		
+		//First class
+		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[9]/td[2]/font/font/input[2]"));
+		assist.click();
+		
+		//Arriving to
+		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[10]/td[2]/select"));
+		assist.click();
+		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[10]/td[2]/select/option[3]"));
+		assist.click();
+		Thread.sleep(1000);
+			
+		//Continue
+		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[14]/td/input"));
+		assist.click();
+		Thread.sleep(1000);
+		
+		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr[1]/td[2]/table/tbody/tr[1]/td/p/font/b/font[1]"));
+		assertEquals("After flight finder - No Seats Avaialble  ", assist.getText());
+		Thread.sleep(1000);
+    }
+	
+	@After
+	public void tearDown() {
+        driver.close();
+    }
 
 }
-
-	
-//private WebDriver driver;
-//	
-//	@Before
-//	public void setup() {
-//		 System.setProperty("webdriver.chrome.driver", "C:\\Users\\e13cr\\Documents\\workspace-spring-tool-suite-4-4.8.0.RELEASE\\selenium-day1\\src\\test\\resources\\driver\\chromedriver.exe");
-//	        driver = new ChromeDriver();
-//	        driver.manage().window().setSize(new Dimension(1366, 768));
-//	}
-//	
-//	@Test
-//	public void test() throws InterruptedException {
-//        WebElement assist;
-//		
-//        String name = "Elena";
-//        String password = "12345";
-//		driver.get("http://demo.guru99.com/test/newtours/");
-//
-//		//Find create user
-//		@FindBy (xpath = "/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td[2]/a")
-//		private WebElement createUserLink;
-//		Thread.sleep(1500);
-//		
-//			
-//        //Username
-//		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[13]/td[2]/input"));
-//		assist.sendKeys(name);
-//		Thread.sleep(1000);
-//		
-//		//Password
-//		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[14]/td[2]/input"));
-//		assist.sendKeys(password);
-//		Thread.sleep(1000);
-//		
-//		//Confirm Password
-//		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[15]/td[2]/input"));
-//		assist.sendKeys(password);
-//		Thread.sleep(1000);
-//		
-//		//Submit
-//		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[17]/td/input"));
-//		assist.click();
-//		Thread.sleep(1000);
-//		
-//		//Sign in
-//		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[3]/td/p[2]/font/a"));
-//		assist.click();
-//		Thread.sleep(1000);
-//		
-//		//Username
-//		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[1]/td[2]/input"));
-//		assist.sendKeys(name);
-//		Thread.sleep(1000);
-//		
-//		//Password
-//		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[2]/td[2]/input"));
-//		assist.sendKeys(password);
-//		Thread.sleep(1000);
-//		
-//		//Submit
-//		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[4]/td/input"));
-//		assist.click();
-//		Thread.sleep(1000);
-//
-//		//Flight finder
-//		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[1]/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[2]/td[2]/a"));
-//		assist.click();
-//		Thread.sleep(1000);
-//		
-//		//Number of passengers
-//		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[3]/td[2]/b/select"));
-//		assist.click();
-//		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[3]/td[2]/b/select/option[2]"));
-//		assist.click();
-//		Thread.sleep(1000);
-//		
-//		//Departure from
-//		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[5]/td[2]/select[1]"));
-//		assist.click();
-//		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[4]/td[2]/select/option[3]"));
-//		assist.click();
-//		Thread.sleep(1000);
-//		
-//		//Arriving to
-//		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[6]/td[2]/select"));
-//		assist.click();
-//		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[6]/td[2]/select/option[5]"));
-//		assist.click();
-//		Thread.sleep(1000);
-//		
-//		//First class
-//		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[9]/td[2]/font/font/input[2]"));
-//		assist.click();
-//		
-//		//Arriving to
-//		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[10]/td[2]/select"));
-//		assist.click();
-//		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[10]/td[2]/select/option[3]"));
-//		assist.click();
-//		Thread.sleep(1000);
-//			
-//		//Continue
-//		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[14]/td/input"));
-//		assist.click();
-//		Thread.sleep(1000);
-//		
-//		assist = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr[1]/td[2]/table/tbody/tr[1]/td/p/font/b/font[1]"));
-//		assertEquals("After flight finder - No Seats Avaialble  ", assist.getText());
-//		Thread.sleep(1000);
-//    }
-//	
-//	@After
-//	public void tearDown() {
-//        driver.close();
-//    }
-//
-//}
